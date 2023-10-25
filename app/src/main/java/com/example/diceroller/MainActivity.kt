6fb094ffba2bcc2a27ac3aa66881a265afa5d4b6
Lê Kỳ Anh <35425267+Cyanh56709@users.noreply.github.com>
2023-10-25 23:12:55 +0700

    Commit bai tap 25/10

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,20 +2,36 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainlayout)
-        val rollButton: Button = findViewById(R.id.roll_button)
+        setContentView(R.layout.activity_main)
 
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView2)
-            val randomNumber = (1..6).random()
-            resultTextView.text = randomNumber.toString()
+        val editTextMoney = findViewById<EditText>(R.id.editTextMoney)
+        val buttonAmazing = findViewById<Button>(R.id.buttonAmazing)
+        val buttonGood = findViewById<Button>(R.id.buttonGood)
+        val buttonOkay = findViewById<Button>(R.id.buttonOkay)
+        val buttonCalculate = findViewById<Button>(R.id.buttonCalculate)
+
+        var tipPercentage = 0.0
+
+        buttonAmazing.setOnClickListener { tipPercentage = 0.20 }
+        buttonGood.setOnClickListener { tipPercentage = 0.18 }
+        buttonOkay.setOnClickListener { tipPercentage = 0.15 }
+
+        buttonCalculate.setOnClickListener {
+            val money = editTextMoney.text.toString().toDoubleOrNull()
+            if (money != null) {
+                val tip = money * tipPercentage
+                Toast.makeText(this, "Tip amount: $tip", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Please enter a valid amount", Toast.LENGTH_LONG).show()
+            }
         }
     }
-
 }
